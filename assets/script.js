f77d0a5c7fe7e258cd870b02dc34aa4ba8413282
Christopher <leechrs1@gmail.com>
2023-03-25 23:13:48 -0500

    able to assign factions to players. As of right now results[0] and [1] are assigned to player1, results[2] and results[3] get assigned to player2 and so on

--- assets/script.js
+++ assets/script.js
@@ -24,33 +24,126 @@ const cthulu = [
     'Miskatonic University'
 ]
 
-const misc = [
-    'Sheep',
+const sciFi = [
+    'Cyborg Apes',
+    'Shapeshifters',
+    'Super Spies',
+    'Time Travelers'
+]
+
+const monsterSmash = [
+    'Giant Ants',
+    'Mad Scientists',
+    'Vampires',
+    'Werewolves'
+]
+
+const pretty = [
+    'Fairies',
+    'Kitty Cats',
+    'Mythic Horses',
+    'Princesses'
+]
+
+const yourFault = [
+    'Dragons',
+    'Mythic Greeks',
+    'Sharks',
+    'Superheroes',
+    'Tornados'
+]
+
+const ceaseDesist = [
+    'Astroknights',
+    'Changerbots',
+    'Ignobles',
+    'Star Roamers'
+]
+
+const thinking = [
+    'Explorers',
+    'Grannies',
+    'Rock Stars',
+    'Teddy Bears'
+]
+
+const japan = [
+    'Itty Critters',
+    'Kaiju',
+    'Magical Girls',
+    'Mega Troopers'
+]
+
+const sheep = [
+    'Sheep'
+]
+
+const allStars = [
     'All Stars'
 ]
 
+// Empty arrays
+let expansions = []
 
+// Get number of players and player arrays
+let player1 = [];
+let player2 = [];
+let player3 = [];
+let player4 = [];
 
-//Selected sets are placed in an array
-function selectedSets() {
-    let arr = [];
-    let checkboxes = document.querySelectorAll("input[type='checkbox']:checked");
-    for (let i = 0 ; i < checkboxes.length; i++) {
-    arr.push(checkboxes[i].value)
-    }
-
-    let newArr = arr.flatMap(set => set)
-
-    console.log(arr[0]);
+let n 
+function getPlayers() {
+    n = document.getElementById('players').value;
 }
 
-// store selected sets 
-// const gameContents = [
-//     coreSet,
-//     awesomeLevel,
-//     cthulu,
-//     misc
-//   ];
+
+function ifSelected() {
+    let coreSetCheckBox = document.getElementById("coreSet");
+    let awesomeLevelCheckBox = document.getElementById("awesomeLevel9000");
+    let cthuluCheckBox = document.getElementById("cthulu");
+    let sheepCheckBox = document.getElementById("sheep");
+    let allStarsCheckBox = document.getElementById("allStars");
+
+    // Check to see if expansion has been checked. If so, add expansion to expansions array
+    if (coreSetCheckBox.checked) {
+        expansions.push(coreSet)
+    };
+    if (awesomeLevelCheckBox.checked) {
+        expansions.push(awesomeLevel)
+    };
+    if (cthuluCheckBox.checked) {
+        expansions.push(cthulu)
+    };
+    if (sheepCheckBox.checked) {
+        expansions.push(sheep)
+    };
+    if (allStarsCheckBox.checked) {
+        expansions.push(allStars)
+    };
+
+    // merge all elements in array in to one array
+    let flattened = expansions.flatMap(factions => factions);
+    console.log(flattened);
+
+    // shuffle flattened array and choose n * 2 elements from the shuffledArray
+    let shuffledArray = flattened.sort(() => 0.5 - Math.random())
+    let results = shuffledArray.slice(0, n * 2);
+
+    console.log(results)
+    console.log(n + " players are playing")
+
+    // factions get distributed to player
+   if (n == 2) {
+    player1.push(results[0])
+    player1.push(results[1])
+    player2.push(results[2])
+    player2.push(results[3])
+   }
+
+   console.log('Player 1 gets ' + results[0] + ' and ' + results[1])
+   console.log('Player 2 gets ' + results[2] + ' and ' + results[3])
+} 
+
 
 // const chooseRandom = (gameContents, num = 1) => {
 //     const results = [];
